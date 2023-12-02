d3771c95d7e8eeed8b282c9cbc79f9405c8dc7be
huseinzol05 <husein.zol05@gmail.com>
2023-12-02 23:35:07 +0800

    added mistral contrastive

---
+++ run-mistral-191m-contrastive.sh
@@ -0,0 +1,24 @@
+WANDB_PROJECT=mistral-embedding-191m-contrastive \
+deepspeed run_contrastive.py \
+--deepspeed ds_config_zero3.json \
+--output_dir="./embedding-model-llama-191m-contrastive" \
+--model_name_or_path="mesolitica/malaysian-mistral-191M-4096" \
+--train_data="/home/ubuntu/mosaic-embedding-pairs" \
+--per_device_train_batch_size="8" \
+--gradient_accumulation_steps 2 \
+--learning_rate="2e-5" \
+--num_train_epochs="2" \
+--max_seq_length 8192 \
+--save_steps="300" \
+--save_total_limit="3" \
+--do_train \
+--gradient_checkpointing \
+--logging_steps 1 \
+--normalized True \
+--temperature 0.02 \
+--query_max_len 8192 \
+--passage_max_len 8192 \
+--train_group_size 3  \
+--sentence_pooling_method="mean" \
+--max_grad_norm 0.5 \
+--bf16
